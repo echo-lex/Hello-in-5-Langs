63a6b83ca0d8710b7e984a7453b937d507f225cf
Lex <162380158+echo-lex@users.noreply.github.com>
2024-04-11 12:46:08 -0600

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+//This one was slightly tricky for me to get it to run, but I was looking at ways to run JavaScript instead of Java
+
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
