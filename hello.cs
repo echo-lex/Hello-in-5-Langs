9a0f9f6768250efbca0fb4c08d02a325cf498d3a
Lex <162380158+echo-lex@users.noreply.github.com>
2024-04-11 13:57:51 -0600

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,11 @@
+//I wanted to complete the trifecta of C languages
+
+namespace helloWorld
+{
+    class Hello {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
